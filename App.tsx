3a2281602868d411fbf979517f50393fc6c2abb0
Samuel Santos <samuellima280499@gmail.com>
2021-11-29 23:01:59 -0300

    Add splash screen

--- App.tsx
+++ App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Routes  } from './src/routes';
+import { useFonts, Ubuntu_400Regular, Ubuntu_700Bold, Ubuntu_500Medium } from '@expo-google-fonts/ubuntu';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Ubuntu_400Regular,
+    Ubuntu_500Medium,
+    Ubuntu_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <>
       <StatusBar translucent />
